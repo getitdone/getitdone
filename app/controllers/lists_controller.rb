98b5634f0296fb2b024352518731f5c61cf81e9b
Justin Chen <justin@twitter.com>
2010-09-13 01:12:10 -0700

    removed some stuff from git repo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,10 +9,20 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new params[:post]
+    @list = List.new params[:list]
     if @list.save
+      puts "foo"
       redirect_to lists_path, :notice => "New list created"
     else
+      puts "didn't work"
       render :action => "new"
     end
+  end
+
+  def destroy
+    @list = List.find params[:id]
+    @list.destroy
+
+    redirect_to lists_path, :notice => "List destroyed"
+  end
 end
