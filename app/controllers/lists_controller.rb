f80fb617b5da5720423ab486d676c036fe2bc9db
Justin Chen <iareloser@gmail.com>
2010-09-12 20:36:51 -0700

    trying to add Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,14 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new params[:post]
+
+    if @list.save
+      redirect_to lists_path, :notice => "New list created"
+    else
+      render :action => "new"
+    end
+  end
+
 end
